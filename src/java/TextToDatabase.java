2851d4c44a4530a28ae268d050c9414173d1f3b9
Tiffany <Tiffany@Tiffany-THINK>
2018-03-01 23:02:58 -0500

    Hint is now able to receive values

--- src/java/TextToDatabase.java
+++ src/java/TextToDatabase.java
@@ -1,8 +1,6 @@
 
-import com.mysql.jdbc.PreparedStatement;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.sql.*;
+import java.io.*;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -16,6 +14,7 @@ import java.io.InputStreamReader;
 public class TextToDatabase {
 
     private static PreparedStatement insert;
+    private static Connection conn;
     static String line, question = "", choiA = "", choiB = "",
             choiC = "", choiD = "", choiE = "", ans = "", hint = "";
     private static int chapNo, length;
@@ -23,7 +22,7 @@ public class TextToDatabase {
     public static void main(String[] args) {
         try {
             //loop through each chapter
-            for (chapNo = 2; chapNo < 3; chapNo++) {
+            for (chapNo = 1; chapNo < 2; chapNo++) {
                 int quesNo = 1;
                 String fName = "C:\\Users\\Tiffany\\Documents\\College\\CompSci\\rapid\\questions\\chapter" + chapNo + ".txt";
                 FileInputStream fstream = new FileInputStream(fName);
@@ -82,6 +81,7 @@ public class TextToDatabase {
                                         space = length;
                                     }
                                     ans = strLine.substring(4, space);
+                                    hint = strLine.substring(space, length);
                                 }
 
                                 break;
@@ -90,7 +90,10 @@ public class TextToDatabase {
                                 question += "\n" + strLine;
                             }
                         }
-
+                        //insert statement here
+                        String stmt = "insert into tran(chapNo, "
+                    + "SecNo, question, choiA, choiB, choiC, choiD, choiE, ans,"
+                    + " hint) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
                         System.out.println(quesNo + ") " + question);
                         System.out.println("a. " + choiA);
                         System.out.println("b. " + choiB);
@@ -98,6 +101,7 @@ public class TextToDatabase {
                         System.out.println("d. " + choiD);
                         System.out.println("e. " + choiE);
                         System.out.println("Key:" + ans);
+                        System.out.println("Hint: " + hint);
 
                     }
                     if (strLine.startsWith("#")) {
@@ -114,11 +118,28 @@ public class TextToDatabase {
 
                 }
 
-                //insert statement here
             }
 
         } catch (Exception ex) {
             System.out.println(ex);
+        }
+
+    }
+
+    private void initializeJdbc() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded");
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/tran", "tran", "tiger");
+            System.out.println("Database connected");
+
+            insert = connection.prepareStatement("insert into tran(chapNo, "
+                    + "SecNo, question, choiA, choiB, choiC, choiD, choiE, ans,"
+                    + " hint) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+        } catch (ClassNotFoundException | SQLException x) {
+            System.out.println(x);
         }
 
     }
